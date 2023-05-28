8028888795b11797c30483042dc0843c0ebfed1f
Ly Tuan Kiet <lyhung998877@gmail.com>
2023-05-28 15:27:52 +0700

    added gitignore

---
+++ src/utilities/swagger.js
@@ -0,0 +1,24 @@
+const config = require("../configs");
+const swaggerJSDoc = require("swagger-jsdoc");
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Ministore API",
+      version: "1.0.0",
+      description: "Ministore API with Swagger",
+    },
+    servers: [
+      {
+        url: `http://${config.url}:${config.port}`,
+        description: "Ministore API server",
+      },
+    ],
+    
+  },
+  apis: ["../routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
