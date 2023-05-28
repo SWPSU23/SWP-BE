e0c4de4b3fac948304995c50528f246028b2839a
Ly Tuan Kiet <lyhung998877@gmail.com>
2023-05-28 17:13:44 +0700

    recover

--- src/controllers/employeesController.js
+++ src/controllers/employeesController.js
@@ -1,34 +1,44 @@
-const employeesModel = require('../models/employeesModel');
+const employeesModel = require("../models/employeesModel");
 
 const createEmployeeDetail = (req, res) => {
-    try {
-        const check = employeesModel.createEmployeeDetail(req.body);
-        if (check) {
-            res.status(200).json({
-                status: 'success',
-                message: 'Employee Detail created successfully',
-            });
-        } else {
-            throw new Error();
-        }
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+  try {
+    const check = employeesModel.createEmployeeDetail(req.body);
+    if (check) {
+      res.status(200).json({
+        status: "success",
+        message: "Employee Detail created successfully",
+      });
+    } else {
+      throw new Error();
     }
-}
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 const getListEmployee = async (req, res) => {
-    try {
-        const listProduct = await employeesModel.getListEmployee();
-        res.status(200).json({
-            status: 'success',
-            message: 'Employee List created successfully',
-            data: listProduct,
-        })
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+  try {
+    const listProduct = await employeesModel.getListEmployee();
+    res.status(200).json({
+      status: "success",
+      message: "Employee List created successfully",
+      data: listProduct,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+const getEmployeeDetail = async (req, res) => {
+  try {
+    const listProduct = await employeesModel.getEmployeeDetail(req.params.id);
+    res.status(200).json({
+      status: "success",
+      message: "Employee List created successfully",
+      data: listProduct,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
-}
-
-
-module.exports = { createEmployeeDetail, getListEmployee };
+module.exports = { createEmployeeDetail, getListEmployee, getEmployeeDetail };
