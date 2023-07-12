0d84249671b7d4c794600e6405677a26bada167e
Ly Tuan Kiet <lyhung998877@gmail.com>
2023-07-12 17:41:58 +0700

    improve logging

--- src/utilities/logger.js
+++ src/utilities/logger.js
@@ -2,14 +2,22 @@ const winston = require('winston')
 const time = require('./timeHelper')
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-    ),
+    format: winston.format.combine(winston.format.json()),
     transports: [
-        new winston.transports.Console(),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            ),
+        }),
         new winston.transports.File({
             filename: `./logs/${time.getNowDate()}.log`,
+            format: winston.format.combine(
+                winston.format.timestamp({
+                    format: 'YYYY-MM-DD HH:mm:ss',
+                }),
+                winston.format.json()
+            ),
         }),
     ],
 })
