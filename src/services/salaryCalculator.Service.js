f70e916ef41316963592ded9fe789acbdba62035
Ly Tuan Kiet <lyhung998877@gmail.com>
2023-07-20 19:56:46 +0700

    added getUserInfo method

---
+++ src/services/salaryCalculator.Service.js
@@ -0,0 +1,12 @@
+const calculateSalaryByWorksheet = async (employee_id, worksheet_id) => {
+
+}
+
+const calculateTotalSalary = async () => {
+    
+}
+module.exports = {
+    calculateSalaryByWorksheet,
+    calculateSalaryByMonth,
+    exportPayroll,
+}
