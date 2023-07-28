30d65155de7d05ba6d7c7fcfc40bd28fbf2995c1
Huynh Chi Bao <baohc110902@gmail.com>
2023-07-29 03:46:34 +0700

    create dashboard

--- src/services/dashboard.Service.js
+++ src/services/dashboard.Service.js
@@ -191,20 +191,197 @@ const dashboardSchema = {
 }
 const readDashboard = async () => {
     // calculate revenue of 7days before include current day
-    const last_seven_days_revenue = await calculateLastSevenDaysRevenue()
+    const last_seven_days_revenue = await calculateLastSevenDaysRevenue();
+    // calculate top 3 best selling product of 7days before
+    const top_three_best_selling_product_data = await calculateTopThreeBestSellingProductData();
+    // calculate orders of 7days before
+    const order_last_seven_days_data = await calculateOrderLastSevenDaysData();
+    // calculate Top 10 best selling products of 7days before
+    const weekly_best_selling_product_data = await calculateTenBestSellingProduct();
+    // calculate today data
+    const data = {
+        last_seven_days_revenue: last_seven_days_revenue,
+        top_three_best_selling_product_data: top_three_best_selling_product_data,
+        order_last_seven_days_data: order_last_seven_days_data,
+        weekly_best_selling_product_data: weekly_best_selling_product_data,
+        today_data: 'kiệt làm cái này'
+    }
+    return data;
 }
+
 const calculateLastSevenDaysRevenue = async () => {
     // get current date
-    const currentDate = time.getNowDate()
+    const currentDate = time.getNowDate();
     // get 6 days before
-    const sixDaysBefore = time.getBeforeDate(time.timeStampToDate(currentDate), 6)
+    const sixDaysBefore = time.getBeforeDate(currentDate, 6);
     // get all order from 6 days before to current date
     const orders = await query.getData(
         `SELECT * FROM \`Order\` WHERE \`create_at\` BETWEEN '${sixDaysBefore} 00:00:00' AND '${currentDate} 23:59:59'`,
         []
     )
-    global.logger.info(JSON.stringify(orders))
+    // calculate revenue of each day
+    const last_seven_days_revenue = [];
+    for (let i = 0; i < 7; i++) {
+        const day = time.getBeforeDate(currentDate, 6 - i);
+        const revenue = calculateRevenueOfDay(day, orders);
+        last_seven_days_revenue.push({
+            id: i + 1,
+            day: day,
+            revenue: revenue,
+        })
+    }
+    return last_seven_days_revenue;
 }
+
+const calculateRevenueOfDay = (day, orders) => {
+    let revenue = 0;
+    for (let i = 0; i < orders.length; i++) {
+        if (time.timeStampToDate(orders[i].create_at) === day) {
+            revenue += orders[i].total_price;
+        }
+    }
+    return revenue;
+}
+
+const calculateBestSellingProduct = async () => {
+    // get current date
+    const currentDate = time.getNowDate();
+    // get 6 days before
+    const sixDaysBefore = time.getBeforeDate(currentDate, 6);
+    // get all order from 6 days before to current date
+    const list_order = await query.getData(
+        `SELECT OP.product_id, OP.total FROM \`Order\` o` +
+        ` JOIN \`OrderProduct\` OP ON o.id = OP.order_id` +
+        ` WHERE o.\`create_at\` BETWEEN '${sixDaysBefore} 00:00:00' AND '${currentDate} 23:59:59'`,
+        []
+    );
+    // calculate total sold of each product
+    const list_product = [];
+    for (let i = 0; i < list_order.length; i++) {
+        const product_id = list_order[i].product_id;
+        const total_sold = list_order[i].total;
+        let isExist = false;
+        for (let j = 0; j < list_product.length; j++) {
+            if (list_product[j].product_id === product_id) {
+                list_product[j].total_sold += total_sold;
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            list_product.push({
+                product_id: product_id,
+                total_sold: total_sold,
+            })
+        }
+    }
+    // sort list product by total sold
+    list_product.sort((a, b) => {
+        return b.total_sold - a.total_sold;
+    })
+    return list_product;
+}
+
+const calculateTopThreeBestSellingProductData = async () => {
+    const list_product = await calculateBestSellingProduct();
+    const top_three_best_selling_product_data = [];
+    const total_revenue = await getTotalRevenueOfWeek();
+    for (let i = 0; i < 3; i++) {
+        if (i < list_product.length) {
+            const product = await query.getData(
+                `SELECT * FROM \`Product\` WHERE \`id\` = ?`,
+                [list_product[i].product_id]
+            )
+            top_three_best_selling_product_data.push({
+                id: i + 1,
+                product: product[0].name,
+                sold: list_product[i].total_sold,
+                percent: (list_product[i].total_sold / total_revenue) * 100,
+            })
+        } else {
+            break;
+        }
+    }
+    return top_three_best_selling_product_data;
+}
+
+const calculateOrderLastSevenDaysData = async () => {
+    // get current date
+    const currentDate = time.getNowDate();
+    // get 6 days before
+    const sixDaysBefore = time.getBeforeDate(currentDate, 6);
+    // get all order from 6 days before to current date
+    const orders = await query.getData(
+        `SELECT * FROM \`Order\` WHERE \`create_at\` BETWEEN '${sixDaysBefore} 00:00:00' AND '${currentDate} 23:59:59'`,
+        []
+    )
+    // calculate total order of each day
+    const order_last_seven_days_data = [];
+    for (let i = 0; i < 7; i++) {
+        const day = time.getBeforeDate(currentDate, 6 - i);
+        const total_order = calculateTotalOrderOfDay(day, orders);
+        order_last_seven_days_data.push({
+            id: i + 1,
+            title: day,
+            total_order: total_order,
+        })
+    }
+    return order_last_seven_days_data;
+}
+
+const calculateTotalOrderOfDay = (day, orders) => {
+    let total_order = 0;
+    for (let i = 0; i < orders.length; i++) {
+        if (time.timeStampToDate(orders[i].create_at) === day) {
+            total_order++;
+        }
+    }
+    return total_order;
+}
+
+const calculateTenBestSellingProduct = async () => {
+    const list_product = await calculateBestSellingProduct();
+    const top_ten_best_selling_product_data = [];
+    const total_revenue = await getTotalRevenueOfWeek();
+    for (let i = 0; i < 10; i++) {
+        if (i < list_product.length) {
+            const product = await query.getData(
+                `SELECT * FROM \`Product\` WHERE \`id\` = ?`,
+                [list_product[i].product_id]
+            )
+            top_ten_best_selling_product_data.push({
+                id: i + 1,
+                product: product[0].name,
+                sold: list_product[i].total_sold,
+                percent: (list_product[i].total_sold / total_revenue) * 100,
+            })
+        } else {
+            break;
+        }
+    }
+    return top_ten_best_selling_product_data;
+
+}
+
+const getTotalRevenueOfWeek = async () => {
+    // get current date
+    const currentDate = time.getNowDate();
+    // get 6 days before
+    const sixDaysBefore = time.getBeforeDate(currentDate, 6);
+    // get all order from 6 days before to current date
+    const orders = await query.getData(
+        `SELECT * FROM \`Order\` WHERE \`create_at\` BETWEEN '${sixDaysBefore} 00:00:00' AND '${currentDate} 23:59:59'`,
+        []
+    )
+    // calculate total order of each day
+    let total_revenue = 0;
+    for (let i = 0; i < orders.length; i++) {
+        total_revenue += orders[i].total_price;
+    }
+
+    return total_revenue;
+}
+
 module.exports = {
     readDashboard,
 }
